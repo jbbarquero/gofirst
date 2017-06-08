c9cafaf6a48c7f6c81fdcdff517f69fa7f44f135
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-06-08 12:51:01 +0200

    Go first: user and admin (interfaces finished)

--- types/user.go
+++ types/user.go
@@ -1,30 +1,54 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 // notifier is an interface that defined notification type behavior.
 type notifier interface {
-  notify()
+	notify()
 }
 
 type user struct {
-  name string
-  email string
+	name  string
+	email string
 }
 
 // notify implements a method with a pointer receiver.
 func (u *user) notify() {
-  fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
+	fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
+}
+
+type admin struct {
+	user
+	level string
+}
+
+// notify implements a method that can be called via a value of type admin
+func (a *admin) notify() {
+	fmt.Printf("Sending ADMIN email to %s<%s>, level: %s\n",
+		a.name, a.email, a.level)
 }
 
 func sendNotification(n notifier) {
-  n.notify()
+	n.notify()
 }
 
 func main() {
-  u := user{"Javier", "javier@correo.es"}
+	u := user{"Javier", "javier@correo.es"}
 
-  sendNotification(&u)
+	sendNotification(&u)
+
+	ad := admin{
+		user: user{
+			name:  "Admin",
+			email: "admin@correo.es",
+		},
+		level: "super-power",
+	}
+
+	ad.user.notify()
+
+	sendNotification(&ad)
+
 }
