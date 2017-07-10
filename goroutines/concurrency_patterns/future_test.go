af1c306ad40d950fda96354ef88de743d825ba1f
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-07-10 18:42:22 +0200

    Go first: concurrent patterns, future (just copying the code from the video)

---
+++ goroutines/concurrency_patterns/future_test.go
@@ -0,0 +1,84 @@
+package concurrency_patterns
+
+import (
+  "errors"
+  "sync"
+  "testing"
+  "time"
+)
+
+/*
+GEMMA BENEITO
+PAPA
+PAPI
+*/
+
+func TestStringOrError_Execute(t *testing.T)  {
+  future := &MaybeString{}
+
+  t.Run("Success result", func (t *testing.T)  {
+    var wg sync.WaitGroup
+    wg.Add(1)
+
+    //Timeout
+    go timeout(t, &wg)
+    /*
+    future.Success(func (s string)  {
+      t.Log(s)
+      wg.Done()
+    }).Fail(func (e error)  {
+      t.Fail()
+      wg.Done()
+    })
+    future.Execute(func () (string, error)  {
+      return "Hello World", nil
+    })
+    wg.Wait()
+    */
+  })
+
+  t.Run("Failed result", func (t *testing.T)  {
+    var wg sync.WaitGroup
+    wg.Add(1)
+    future.Success(func (s string)  {
+      t.Fail()
+      wg.Done()
+    }).Fail(func (e error)  {
+      t.Log(e.Error())
+      wg.Done()
+    })
+    future.Execute(func () (string, error)  {
+      return "", errors.New("Error ocurred")
+    })
+    wg.Wait()
+  })
+
+  t.Run("Closure Success result", func (t *testing.T)  {
+    var wg sync.WaitGroup
+    wg.Add(1)
+
+    go timeout(t, &wg)
+
+    future.Success(func (s string)  {
+      t.Log(s)
+      wg.Done()
+    }).Fail(func (e error)  {
+      t.Fail()
+      wg.Done()
+    })
+
+    future.Execute(setContext("Hello"))
+
+    wg.Wait()
+
+  })
+
+}
+
+func timeout(t *testing.T, wg *sync.WaitGroup)  {
+  time.Sleep(time.Second)
+  t.Log("Timeout!")
+  t.Fail()
+  wg.Done()
+
+}
